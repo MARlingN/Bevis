97d24ac6b96184662273a677b9d432c873636eba
MARlingN <Schumibest@gmail.com>
2017-04-03 10:25:57 +0200

    Her er starten.

---
+++ Bevis/Bevis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tjek4 tjek = new tjek4();
+            int a = 4;
+            int b = 8;
+            int c = b / a;
+            int d = 2;
+            Console.WriteLine(tjek.erdet(d, c));
+            Console.WriteLine(tjek.erdet(a, b));
+            Console.WriteLine(tjek.erdet(1, 3));
+        }
+    }
+}
---
+++ Bevis/Bevis/tjek4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevis
+{
+    class tjek4
+    {
+        public string erdet(int a, int b)
+        {
+            string ord = "no";
+            int c = a + b;
+            if (c == 4)
+            {
+                ord = "yes";
+            }
+            return ord;
+        }
+    }
+}
